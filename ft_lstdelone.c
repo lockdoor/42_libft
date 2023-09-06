7c6ca9e87d15c7d7c6c7a9d0bbda0eedc4856190
Pitsanu Namnil <pnamnil@z2t5c1.42bangkok.com>
2023-09-06 08:55:57 +0700

    fixed ft_lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: pnamnil <pnamnil@student.42bangkok.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 14:40:50 by pnamnil           #+#    #+#             */
-/*   Updated: 2023/09/06 07:39:57 by pnamnil          ###   ########.fr       */
+/*   Updated: 2023/09/06 08:53:17 by pnamnil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	if (!lst && !del)
+	if (!lst)
 		return ;
 	if (del)
 		del (lst->content);
